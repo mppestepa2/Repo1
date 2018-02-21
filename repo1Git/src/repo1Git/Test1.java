fb1ea90e1feea1a96d01c30d9227c5dd605be8d2
alumno <alumno@PC18>
2018-02-21 13:48:51 +0100

    Segundo Commit

--- repo1Git/src/repo1Git/Test1.java
+++ repo1Git/src/repo1Git/Test1.java
@@ -3,8 +3,9 @@ package repo1Git;
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("Primera line");
-
+		System.out.println("Primera linea");
+		System.out.println("Segunda linea");
+		
 	}
 
 }
